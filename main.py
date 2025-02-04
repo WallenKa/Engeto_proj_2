4873682d59f7c90a76105b4b0c5292ab64881893
WallenKa <wallenka@icloud.com>
2025-02-04 19:07:47 +0100

    Fixed usage of global variables, added if cases for wrong inputs, added formatting.

--- main.py
+++ main.py
@@ -9,19 +9,11 @@ email: wallenka@icloud.com
 # aim is to guess 4 digit generated number, where the digits can't repeat and first can't be 0
 import random
 
-number = []
-chosen_number = ""
-attempts = 0
-cows = 0
-bulls = 0
-
-# list s cislama
-# chci nahodne vytvorit cislo a pridat ho k listu, pokud tam jeste neni
-# pokud je na indexu 0 0, musi vymyslet jine co neni nula
-
 def underscore():
     print("-" * 47)
+
 def generate_number():
+    number = []
     while len(number) < 4:
         x = random.randrange(0,9)
         if x not in number:
@@ -30,17 +22,13 @@ def generate_number():
             y = random.randrange(1,9)
             number.insert(0, y)
     print(number)
+    return number
 
-def game():
-    global attempts
-    global chosen_number
-    attempts += 1  # adds number of attempts each turn
-    cows = 0
-    global bulls
+def game(number, attempts):
     guess = []
-
     cows = 0
     bulls = 0
+    attempts += 1
     chosen_number = "0"
     while len(chosen_number) != 4 or chosen_number.isnumeric() is False:
         print("Enter a number:")
@@ -59,10 +47,24 @@ def game():
         elif guess[x] in number and guess[x] != number[x]:
             cows += 1
     if bulls == 4:
-        print(f"Correct, you've guessed the right number in {attempts} guesses!")
+        if attempts == 1:
+            print(f"Correct, you've guessed the right number in {attempts} guess!")
+        else:
+            print(f"Correct, you've guessed the right number in {attempts} guesses!")
+        underscore()
+        print("That's amazing!")
     elif bulls <4:
         print(f"{bulls} bulls, {cows} cows")
+    return attempts, bulls
 
-generate_number()
+number = generate_number()
+attempts = 0
+bulls = 0
+print("Hi there!")
+underscore()
+print("I've generated a random 4 digit number for you.\nLet's play a bulls and cows game.")
+underscore()
 while bulls != 4:
-    game() # jak udelat, aby to bylo v loopu
+    attempts, bulls = game(number, attempts)
+
+
